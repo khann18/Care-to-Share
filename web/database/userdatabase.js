802baa640b157ff63f945b39eb8a30d509932447
khann18 <khann22@seas.upenn.edu>
2020-04-05 16:57:29 -0700

    Fixed master compilation (not the JSON thing)
    Hi please fix JSON thing maddie

--- web/database/userdatabase.js
+++ web/database/userdatabase.js
@@ -14,10 +14,6 @@ var createUser = function(user, route_callback) {
 	    } );
 }
 
-var getPassword = function(username, route_callback) {
-	User.find({username : username}).select('password').exec(route_callback);
-}
-
 var getUser = function(username, route_callback) {
 	console.log("Finding Users")
 	User.find().exec(route_callback);
